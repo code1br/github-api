f1b8f1dd2f7eb51d6e3b1456b4f1ff30654f4476
Eliezer Marques Mafra <32603174+EliezerMafra@users.noreply.github.com>
2022-09-27 10:14:08 -0300

    adding user token verification

---
+++ src/middlewares/user-authentication.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express'
+import { Buffer } from 'buffer'
+import { githubApi } from '../apis/github-api'
+
+async function Authenticate(req: Request, res: Response, next: NextFunction){
+	const headerAuth = req.headers.authorization
+
+	if(headerAuth){
+		const auth = Buffer.from(headerAuth.split(' ')[1], 'base64').toString()
+
+		const username = auth.split(':')[0]
+		const token = auth.split(':')[1]
+
+		try{
+			await githubApi.get('/user', {
+				auth:{
+					username: username,
+					password: token	
+				}
+			})
+
+			return next
+		}catch(err){
+			res.status(401).send(`Error while authenticating: ${err}`)
+		}
+
+	}else{
+		res.status(401).send(`Basic auth required`)
+	}
+
+}
+
+export { Authenticate }
