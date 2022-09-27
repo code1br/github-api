d574c317f9b819b9f982baf4b357ba53a543075d
Eliezer Marques Mafra <32603174+EliezerMafra@users.noreply.github.com>
2022-09-27 08:14:04 -0300

    Express config and folder structure

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log(`Hello World`)
+import { app } from "./app"
+import 'dotenv/config'
+
+const server_host = process.env.SERVER_HOST || 'localhost'
+const server_port = parseInt(process.env.SERVER_PORT || '6060')
+
+app.listen(server_port, server_host, () => {
+	console.log(`Listening on http://${server_host}:${server_port}`)
+})
