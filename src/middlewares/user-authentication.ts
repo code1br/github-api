6e0e2856b017ccfef12a6aa9b25b965c93051a12
Xênia Barreto <xenia.barreto@queimadiaria.com.br>
2022-10-13 16:20:33 -0300

    update Authenticate

--- src/middlewares/user-authentication.ts
+++ src/middlewares/user-authentication.ts
@@ -9,35 +9,53 @@ let CURRENT_USER: UserModel = {
 }
 
 async function Authenticate(req: Request, res: Response, next: NextFunction) {
-	const headerAuth = req.headers.authorization
+	const headerAuth = req.headers.["authorization"];
+	const token = headerAuth && headerAuth.split(" ")[1];
 
-	if (headerAuth) {
-		const auth = Buffer.from(headerAuth.split(' ')[1], 'base64').toString()
-
-		const login = auth.split(':')[0]
-		const token = auth.split(':')[1]
-
-		try {
-			const result = await githubApi.get('/user', {
-				auth: {
-					username: login,
-					password: token
-				}
-			})
-
-			CURRENT_USER.login = login
-			CURRENT_USER.PAT = token
-
-			return next()
-		} catch (err) {
-			res.status(401).send(`Error while authenticating: ${err}`)
-		}
-
-	} else {
-		res.status(401).send(`Basic auth required`)
+	if(!token){
+		return res.status(401).json({
+			message: "Access denied"
+		});
 	}
 
-}
+	try{
+		const jwt_secret = process.env.JWT_SECRET;
+		jwt.verify(token, jwt_secret);
+		next();
+	}catch(error){
+		return res.status(500).json({
+			message: "Insert a valid token!"
+		});
+	}
+
+
+// 	if (headerAuth) {
+// 		const auth = Buffer.from(headerAuth.split(' ')[1], 'base64').toString()
+
+// 		const login = auth.split(':')[0]
+// 		const token = auth.split(':')[1]
+
+// 		try {
+// 			const result = await githubApi.get('/user', {
+// 				auth: {
+// 					username: login,
+// 					password: token
+// 				}
+// 			})
+
+// 			CURRENT_USER.login = login
+// 			CURRENT_USER.PAT = token
+
+// 			return next()
+// 		} catch (err) {
+// 			res.status(401).send(`Error while authenticating: ${err}`)
+// 		}
+
+// 	} else {
+// 		res.status(401).send(`Basic auth required`)
+// 	}
+
+// }
 
 export { Authenticate }
 export { CURRENT_USER }
