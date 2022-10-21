abbe73601862db7ae03210a6cea471e2d04fcf4b
David Silveira <david@dsilveira.eti.br>
2022-10-21 11:19:42 -0300

    Add mordan and winston packages to save logs

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
+export const logger = require("./utils/logger");
+export const morganMiddleware = require("./middlewares/morgan.middleware");
 
 export const app = express();
 
+app.use(morganMiddleware);
 app.use(cors());
 app.use(express.json());
 routes(app);
