737a3149d86c8ac5a0b5020bc041244db821a1bf
David Silveira <david@dsilveira.eti.br>
2022-10-26 10:04:41 -0300

    Add morgan and winston packages to save logs (#18)
    * Add mordan and winston packages to save logs
    
    * Remove nodemon package from dependencies

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import 'dotenv/config'
 export const server_host = process.env.SERVER_HOST || 'localhost';
 export const server_ip = process.env.SERVER_IP || 'localhost';
 export const server_port = parseInt(process.env.PORT || '6060');
+export const logger = require("./utils/logger");
 
 app.listen(server_port, server_ip, () => {
-	console.log(`Listening on ${server_host}`);
-	console.log(`Docs on ${server_host}/api-docs`);
+	logger.info(`Listening on ${server_host}`);
 });
---
+++ src/utils/logger.ts
@@ -0,0 +1,36 @@
+const winston = require("winston");
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+}
+
+const level = () => {
+  const env = process.env.NODE_ENV || 'development'
+  const isDevelopment = env === 'development'
+  return isDevelopment ? 'debug' : 'warn'
+}
+
+const transports = [
+  new winston.transports.Console(),
+  new winston.transports.File({
+    filename: 'logs/error.log',
+    level: 'error',
+  }),
+  new winston.transports.File({
+    filename: 'logs/all.log',
+    level: 'http',
+  }),
+]
+
+const logger = winston.createLogger({
+  level: level(),
+  levels,
+  format: winston.format.json(),
+  transports: transports,
+});
+
+module.exports = logger;
